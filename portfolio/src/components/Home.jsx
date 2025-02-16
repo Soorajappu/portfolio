2a45af4af560b1a824770c2929c5c7abe010f022
Soorajappu <soorajkg2002@gmail.com>
2025-02-16 14:52:46 +0530

    portfolio profile photo added and logo changed

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React , { useState } from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 import { Link } from 'react-scroll'
+import ProfilePhoto from '../assets/sooraj.jpg'
 
 function Home() {
 
@@ -11,10 +12,15 @@ function Home() {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
 
+        
+
         {/* container */}
         <div className='max-w-[1000px] mx-auto px-[60px] flex flex-col justify-center h-full'>
+        <div className='p-4'>
+            <img src={ProfilePhoto} alt="Profile Image" className='rounded-full mx-auto w-48 h-48 object-cover'/>
+        </div>
             <p className='text-[#4ca3d8] text-3xl'>Hi, I'm Sooraj K G a passionate Python Django Developer.</p>
-            <p className='text-[#8892b0] py-4  max-w-[700px] text-justify'>
+            <p className='text-[#8892b0] py-4  max-w-[1000px] text-justify'>
                 I specialize in building scalable web applications with Python and Django on the backend. As a recent graduate and a fresher, I'm continuously expanding my skill set to include modern frontend technologies. Currently, I am learning React and Tailwind CSS to create dynamic, responsive user interfaces.
                 With a strong foundation in web development and a growth mindset, I am excited to contribute to innovative projects while enhancing my full-stack development skills.            </p>
             <div>
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-        <div>
-             <img src={Logo} alt='Logo image' style={{width: '50px'}} />
+        <div className="p-2">
+             <img src={Logo} alt='Logo image' style={{width: '80px'}} />
         </div>
         
         {/* Menu */}
